84c49368818bfd65f5815eb7fc3c8f2a6098dc84
Silvestre Luna <slvtrluna@gmail.com>
2019-09-14 11:11:28 -0500

    wrote test that checks for First Name and Last Name

---
+++ NameParser/Parser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NameParser
+{
+    public class Parser
+    {
+        public Name ParseName(string nameToParse)
+        {
+            var splitNames = nameToParse.Split(" ");
+            var name = new Name()
+            {
+                FirstName = splitNames.First(),
+                LastName = splitNames.Last()
+            };
+            return name;
+        }
+    }
+
+
+    public class Name
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
